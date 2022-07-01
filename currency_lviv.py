38ca3d45f205d73cdcabbc0ff9c72b960c2d4b8f
LV-coding <rapax001@gmail.com>
2022-07-01 12:11:45 +0300

    first commit

---
+++ currency_lviv.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+from win10toast import ToastNotifier
+from time import sleep
+from datetime import datetime
+
+def get_currency_prices():
+    
+    currency = ['usd', 'eur', 'pln']
+
+    while True:
+        
+        result = []
+        for curr in currency:
+
+            ua = UserAgent()
+            headers = {
+                'User-Agent':ua.random
+            }
+
+            url = f'https://minfin.com.ua/ua/currency/auction/exchanger/{curr}/buy/lvov/'
+            response = requests.get(url, headers=headers)
+
+            soup = BeautifulSoup(response.content, 'html.parser')
+            for price in soup.find_all('span', class_='Typography cardHeadlineL align'):
+                index = price.text.find(',') + 3
+                result.append(price.text[:index])
+
+        msg = f'{currency[0].upper()}: {result[0]}/{result[1]}\n{currency[1].upper()}: {result[2]}/{result[3]}\n{currency[2].upper()}: {result[4]}/{result[5]}'
+
+        print(datetime.now())
+        print(msg)
+        print('----------------------------------------------')
+
+        toast = ToastNotifier()
+        toast.show_toast(title='Currency prices:', msg=msg, duration=60)
+        sleep(1800)
+
+
+if __name__ == '__main__':
+    get_currency_prices()
